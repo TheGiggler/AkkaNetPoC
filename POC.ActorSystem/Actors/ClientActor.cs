0372660044e8278e038eb15ca23861bbd0d5bde9
Mike Tocci <maddog19rw@gmail.com>
2020-01-31 21:50:11 -0500

    init

---
+++ POC.ActorSystem/Actors/ClientActor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POC.ActorSystem.Actors
+{
+    class ClientActor
+    {
+    }
+}
---
+++ POC.ActorSystem/Messages/TooBusyCallBack.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POC.ActorSystem.Messages
+{
+    class TooBusyCallBack
+    {
+    }
+}
